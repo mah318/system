69784b273a3d22511a5c989c57039437a38ad14e
mah318 <mazhaohong640@gmail.com>
2026-09-07 19:34:46 +0800

    Update 1app.py

--- 1app.py
+++ 1app.py
@@ -16,8 +16,7 @@ BUILTIN_API_KEY = "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"
 # =================================================================-
 
 stock = yf.Ticker(ticker)
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-})
+
 
 def load_data():
     try:
